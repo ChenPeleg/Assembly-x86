ad26f7ed7dfd26d094ca978ef45dd29b2e87a2e7
chenp <chenpeleg@gmail.com>
2023-03-19 18:27:18 +0200

    added layout component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { CoreAppComponent } from './components/app/core-app.component';
+import { CoreAppComponent } from './components/core/core-app.component';
 import {CpuComponent} from "./components/cpu/cpu";
 import {RegisterComponent} from "./components/cpu/register";
 import {MemoryComponent} from "./components/memory/memory";
@@ -9,9 +9,13 @@ import {ExecutionComponent} from "./components/execution/execution";
 import {ConsoleComponent} from "./components/console/console";
 import {AsmEditorComponent} from "./components/asm-editor/asm-editor";
 import {FormsModule} from "@angular/forms";
+import {AppRootComponent} from "./components/root/app-root.component";
+import {LayoutComponent} from "./components/layout/layout.component";
 
 @NgModule({
   declarations: [
+    LayoutComponent,
+    AppRootComponent,
       CoreAppComponent,
       CpuComponent,
       RegisterComponent,
@@ -25,7 +29,7 @@ import {FormsModule} from "@angular/forms";
     FormsModule
   ],
   providers: [],
-  bootstrap: [CoreAppComponent]
+  bootstrap: [AppRootComponent]
 })
 export class AppModule
 {
