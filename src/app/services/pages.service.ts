7d12244f2808a4c2f4abbbb0986cecb1766cf2b6
chenp <chenpeleg@gmail.com>
2023-03-25 16:03:09 +0300

    added markdownToHtml.ts

---
+++ src/app/services/pages.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { firstValueFrom } from "rxjs";
+
+export enum MDFiles {
+  Links = "Links",
+}
+
+@Injectable()
+export class SnackBarService {
+  constructor(private httpClient: HttpClient) {}
+
+  public async getMarkdownText(fileName: MDFiles): Promise<string> {
+    return await firstValueFrom(
+      this.httpClient.get(`assets/markdown/${fileName}`, {
+        responseType: "text",
+      })
+    );
+  }
+}
