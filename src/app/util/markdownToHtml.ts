8c551bc2c074056cf4cec93f7b5b57ba76f49c13
chenp <chenpeleg@gmail.com>
2023-03-25 19:00:54 +0300

    added more styles

--- src/app/util/markdownToHtml.ts
+++ src/app/util/markdownToHtml.ts
@@ -1,4 +1,22 @@
 export const markdownToHTML = (markdown: string): string => {
+  // Convert unordered lists
+  // markdown = markdown.replace(/^\* (.*)$/gm, "<li>$1</li>");
+  // markdown = markdown.replace(/^- (.*)$/gm, "<li>$1</li>");
+  // markdown = markdown.replace(/^(?!<li>)[^\n]*(\n|$)/gm, "<ul>$&</ul>");
+  let mdText = markdown;
+  // mdText = mdText.replace(/^\* (.*)$/gim, "<li>$1</li>");
+  // mdText = mdText.replace(/^\d\. (.*)$/gim, "<li>$1</li>");
+  // mdText = mdText.replace(/<\/li>(\n?<li>)/gim, "</li><li>");
+  // mdText = mdText.replace(/^\s*<li>/gim, "<ul>\n<li>");
+  // mdText = mdText.replace(/<\/li>\s*$/gim, "</li>\n</ul>");
+  // mdText = mdText.replace(/^\s*\*\s*(.*)$/gim, "<ul>\n<li>$1</li>\n</ul>");
+  // mdText = mdText.replace(/^\s*\d\.\s*(.*)$/gim, "<ol>\n<li>$1</li>\n</ol>");
+
+  mdText = mdText.replace(/^\s*-\s*(.*)$/gim, "\n<ul>\n<li>$1</li>\n</ul>");
+  mdText = mdText.replace(/^\s*\*\s*(.*)$/gim, "\n<ul>\n<li>$1</li>\n</ul>");
+  mdText = mdText.replace(/^\s*\d\.\s*(.*)$/gim, "\n<ol>\n<li>$1</li>\n</ol>");
+
+  markdown = mdText;
   // Convert headers (h1, h2, h3)
   markdown = markdown.replace(/^# (.*)$/gm, "<h1>$1</h1>");
   markdown = markdown.replace(/^## (.*)$/gm, "<h2>$1</h2>");
@@ -19,14 +37,8 @@ export const markdownToHTML = (markdown: string): string => {
     /```([\s\S]*?)```/g,
     "<pre><code>$1</code></pre>"
   );
-
-  // Convert unordered lists
-  markdown = markdown.replace(/^\* (.*)$/gm, "<li>$1</li>");
-  markdown = markdown.replace(/^- (.*)$/gm, "<li>$1</li>");
-  markdown = markdown.replace(/^(?!<li>)[^\n]*(\n|$)/gm, "<ul>$&</ul>");
-
   // Convert ordered lists
-  markdown = markdown.replace(/^\d+\. (.*)$/gm, "<li>$1</li>");
+  // markdown = markdown.replace(/^\d+\. (.*)$/gm, "<li>$1</li>");
   // markdown = markdown.replace(/^(?!<li>)[^\n]*(\n|$)/gm, "<ol>$&</ol>");
 
   // Images
@@ -42,8 +54,8 @@ export const markdownToHTML = (markdown: string): string => {
   markdown = markdown.replace(/\`(.*)\`/gim, "<code>$1</code>");
 
   // Paragraphs
-  markdown = markdown.replace(/\n$/gim, "<br />");
-  markdown = markdown.replace(/\n/gim, "<p>$1</p>");
+  // markdown = markdown.replace(/\n$/gim, "<br />");
+  // markdown = markdown.replace(/\n/gim, "<p>$1</p>");
 
   return markdown;
 };
