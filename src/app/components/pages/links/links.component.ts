be2903a0954f2ba2bf87448fd408f17a4a4353d5
chenp <chenpeleg@gmail.com>
2023-03-25 16:09:35 +0300

    added the http client to the pages service

--- src/app/components/pages/links/links.component.ts
+++ src/app/components/pages/links/links.component.ts
@@ -1,13 +1,13 @@
-import {AfterViewInit, Component} from "@angular/core";
+import { AfterViewInit, Component } from "@angular/core";
+import { PagesService } from "../../../services/pages.service";
 
 @Component({
   selector: "app-links",
   templateUrl: "./links.component.html",
-  styleUrls :  ["./links.component.scss"]
+  styleUrls: ["./links.component.scss"],
 })
 export class LinksComponent implements AfterViewInit {
-  constructor() {
-  }
-  ngAfterViewInit() {
-  }
+  constructor(private pagesService: PagesService) {}
+
+  ngAfterViewInit() {}
 }
