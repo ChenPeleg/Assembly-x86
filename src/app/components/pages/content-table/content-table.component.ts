8217e70be591f2e25ff8387e66e813d62633e4fb
chenp <chenpeleg@gmail.com>
2023-05-16 08:23:01 +0300

    added content table component

---
+++ src/app/components/pages/content-table/content-table.component.ts
@@ -0,0 +1,12 @@
+import { AfterViewInit, Component } from "@angular/core";
+
+@Component({
+  selector: "content-table",
+  templateUrl: "./content-table.component.html",
+  styleUrls: ["./content-table.component.scss"],
+})
+export class ContentTableComponent implements AfterViewInit {
+  constructor() {}
+
+  ngAfterViewInit() {}
+}
