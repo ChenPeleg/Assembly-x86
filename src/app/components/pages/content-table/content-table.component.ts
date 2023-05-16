5d8fc7372645ebac3cda7aa85dac17f84ad90f20
chenp <chenpeleg@gmail.com>
2023-05-16 08:53:21 +0300

    created a nested list of content

--- src/app/components/pages/content-table/content-table.component.ts
+++ src/app/components/pages/content-table/content-table.component.ts
@@ -1,12 +1,56 @@
-import { AfterViewInit, Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
+
+interface DocElement {
+  type: "file" | "folder";
+  name: string;
+  fullPath: string[];
+  father: DocElement | null;
+  children: DocElement[];
+}
 
 @Component({
   selector: "content-table",
   templateUrl: "./content-table.component.html",
   styleUrls: ["./content-table.component.scss"],
 })
-export class ContentTableComponent implements AfterViewInit {
+export class ContentTableComponent {
+  public readonly docElement: DocElement = {
+    children: [],
+    father: null,
+    name: "all",
+    fullPath: [],
+    type: "folder",
+  };
+  public pagesNames: string[][] | null = null;
+
   constructor() {}
 
-  ngAfterViewInit() {}
+  @Input("pages") set pages(value: string[][]) {
+    if (this.pagesNames) return;
+    this.pagesNames = value;
+    this.buildNestedDocElement(value);
+  }
+
+  buildNestedDocElement(value: string[][]) {
+    value.forEach((page) => {
+      let lastFather = this.docElement;
+      page.forEach((pagePartName, i) => {
+        let element = lastFather.children.find((c) => c.name === pagePartName);
+        if (element) {
+          lastFather = element;
+        } else {
+          element = {
+            children: [],
+            father: lastFather,
+            fullPath: [],
+            name: pagePartName,
+            type: i === page.length - 1 ? "file" : "folder",
+          };
+          lastFather.children.push(element);
+          lastFather = element;
+        }
+      });
+    });
+    console.log(this.docElement);
+  }
 }
