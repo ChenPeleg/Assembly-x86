bdcea30fb68e417df79fb6686cd7396e177c9fb4
chenp <chenpeleg@gmail.com>
2023-05-17 08:58:36 +0300

    finding tables

--- src/app/util/findMdTables.ts
+++ src/app/util/findMdTables.ts
@@ -1,6 +1,6 @@
 const fromBorderToBorder = /(\n\|([^\n])*\|)*/;
 export const findMdTables = (md: string) => {
   const regex = new RegExp(fromBorderToBorder, "g");
-  const tables = md.match(/(\n\|([^\n])*\|)*/g);
+  const tables = `\n${md}`.match(/(\n\|([^\n])*\|)*/g);
   return tables?.map((t) => t.replace("\n", "")).filter((t) => t);
 };
--- src/app/util/mdTables.spec.ts
+++ src/app/util/mdTables.spec.ts
@@ -1,23 +1,56 @@
 import { findMdTables } from "./findMdTables";
 
-const mdTable = `
-| Bit value | Position value as a power of base 2 | Bit number |
+const mdTable1 = `| Bit value | Position value as a power of base 2 | Bit number |
 |-----------|-------------------------------------|------------|
 | 1         | 128                                 | 7          |
 | 1         | 64                                  | 6          |
 | 1         | 32                                  | 5          |
-| 1         | 16                                  | 4          |
-
-some more text and more text
-
-| Another table | Position value as a power of base 2 | Bit number |
+| 1         | 16                                  | 4          |`;
+const mdTable2 = `| Another table | Position value as a power of base 2 | Bit number |
 |-----------|-------------------------------------|------------|
-| 1         | 128                                 | 7          |
-`;
-
+| 1         | 128                                 | 7          |`;
 describe("Find md tables", () => {
-  it("expect to find a simple table", () => {
-    const result = findMdTables(mdTable);
-    console.log(result);
+  it("expect to find simple table", () => {
+    const result = findMdTables(mdTable1);
+    if (!result) {
+      throw `no results`;
+    }
+    expect(result[0]).toBe(mdTable1);
+  });
+  it("expect to find simple table 2", () => {
+    const result = findMdTables(mdTable2);
+    if (!result) {
+      throw `no results`;
+    }
+    expect(result[0]).toBe(mdTable2);
+  });
+  it("expect to find simple table if there are chars in front of it", () => {
+    const result = findMdTables(`\n abc 123 \n${mdTable2}`);
+    if (!result) {
+      throw `no results`;
+    }
+    expect(result[0]).toBe(mdTable2);
+  });
+  it("expect to find simple table if there are chars in front of it", () => {
+    const result = findMdTables(`\n abc 123 \n${mdTable1}`);
+    if (!result) {
+      throw `no results`;
+    }
+    expect(result[0]).toBe(mdTable1);
+  });
+  it("expect to find simple table if there are chars in after it", () => {
+    const result = findMdTables(`${mdTable1} \n abc 123 \n`);
+    if (!result) {
+      throw `no results`;
+    }
+    expect(result[0]).toBe(mdTable1);
+  });
+  it("expect to find 2 tables if there is a line break between them", () => {
+    const result = findMdTables(`${mdTable1} \n\n${mdTable2}`);
+    if (!result) {
+      throw `no results`;
+    }
+    expect(result[0]).toBe(mdTable1);
+    expect(result[1]).toBe(mdTable2);
   });
 });
