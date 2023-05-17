4308b37313f77927bc79ba8f86c55629fea0ac50
chenp <chenpeleg@gmail.com>
2023-05-17 09:35:33 +0300

    tabels bug

--- src/app/util/mdTableToHtmlTable.ts
+++ src/app/util/mdTableToHtmlTable.ts
@@ -1,30 +1,35 @@
 export const mdTableToHtml = (markdownTable: string) => {
   // Split the Markdown table into rows and columns.
   const rows = markdownTable.split("\n");
-  const columns = rows[0].split("|");
+  const firstRowColumns = rows[0].split("|").filter((c) => c);
 
   // Create an HTML table.
   const table = document.createElement("table");
 
   // Add the table headers.
+  const thead = document.createElement("thead");
   const tHeaderRow = document.createElement("tr");
-  table.appendChild(tHeaderRow);
-  for (const column of columns) {
+  thead.appendChild(tHeaderRow);
+  table.appendChild(thead);
+  for (const column of firstRowColumns) {
     const header = document.createElement("th");
     header.textContent = column;
     tHeaderRow.appendChild(header);
   }
-
+  const tbody = document.createElement("tbody");
+  table.appendChild(tbody);
   // Add the table rows.
+
   for (let i = 1; i < rows.length; i++) {
     const row = rows[i].split("|");
     const tr = document.createElement("tr");
+
     for (const cell of row) {
       const td = document.createElement("td");
       td.textContent = cell;
       tr.appendChild(td);
     }
-    table.appendChild(tr);
+    tbody.appendChild(tr);
   }
 
   return table;
