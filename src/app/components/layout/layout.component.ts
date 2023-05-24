5b3e4c1276355251231b5622b6bf31ff7ba91dde
chenp <chenpeleg@gmail.com>
2023-05-24 17:05:28 +0300

    added the spacer only on specific routs

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -22,9 +22,11 @@ import {
   styleUrls: ["./layout.component.scss"],
 })
 export class LayoutComponent implements OnDestroy, OnInit {
+  private readonly urlsWithoutSpacer = ["\\"];
   private readonly destroy$ = new Subject<void>();
-  public readonly urlPath: Observable<any | null> = this.router.events.pipe(
+  public readonly urlPath: Observable<any> = this.router.events.pipe(
     filter((event) => event instanceof NavigationEnd),
+    map((nav) => (nav instanceof NavigationEnd ? nav.url : "")),
     takeUntil(this.destroy$)
   );
   constructor(private router: Router) {}
@@ -33,5 +35,8 @@ export class LayoutComponent implements OnDestroy, OnInit {
   }
   ngOnInit() {
     this.urlPath.subscribe((e) => console.log(e));
+  }
+  public includeSpacer(url: string): boolean {
+    return this.urlsWithoutSpacer.includes(url);
   }
 }
