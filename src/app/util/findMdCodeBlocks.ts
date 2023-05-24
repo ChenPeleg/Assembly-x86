7bcc4bec370a69298f23743c145bcc5ad4ba5902
chenp <chenpeleg@gmail.com>
2023-05-24 15:34:35 +0300

    all test passed

--- src/app/util/findMdCodeBlocks.ts
+++ src/app/util/findMdCodeBlocks.ts
@@ -1,11 +1,12 @@
-const rgex = /```(?<language>[^\n`]*)(?<code>[^`]*)(?<end>\n```)/g;
+const rgex = /```(?<language>[^\n`]*)(?<code>[^`]*)(?<end>```)/g;
 
 export const findMdCodeBlocks = (md: string) => {
   const codeBlocks = md.match(rgex);
-  console.log(md);
+
   return (codeBlocks || []).map((t) => t.replace("\n", "\n")).filter((t) => t);
 };
 export const mdCodeBlockToHtml = (md: string) => {
   const code = rgex.exec(md)?.groups?.["code"] || "";
+
   return `<pre><code>${code.replace("\n", "")}</code></pre>`;
 };
