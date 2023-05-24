c25959413e23b55cd7ed3293af62651a6116a12c
chenp <chenpeleg@gmail.com>
2023-05-24 16:55:53 +0300

    angular subscribing to the path

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,11 +1,37 @@
-import {Component } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import {
+  ActivatedRoute,
+  Data,
+  NavigationEnd,
+  Router,
+  UrlSegment,
+} from "@angular/router";
+import {
+  distinctUntilChanged,
+  filter,
+  map,
+  Observable,
+  Subject,
+  takeUntil,
+  tap,
+} from "rxjs";
 
 @Component({
   selector: "app-layout",
   templateUrl: "./layout.component.html",
-  styleUrls: ['./layout.component.scss']
+  styleUrls: ["./layout.component.scss"],
 })
-export class LayoutComponent
-{
-
+export class LayoutComponent implements OnDestroy, OnInit {
+  private readonly destroy$ = new Subject<void>();
+  public readonly urlPath: Observable<any | null> = this.router.events.pipe(
+    filter((event) => event instanceof NavigationEnd),
+    takeUntil(this.destroy$)
+  );
+  constructor(private router: Router) {}
+  ngOnDestroy(): void {
+    this.destroy$.complete();
+  }
+  ngOnInit() {
+    this.urlPath.subscribe((e) => console.log(e));
+  }
 }
