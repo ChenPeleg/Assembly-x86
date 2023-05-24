b4af0898d396421b92bbbe8d71a21d65305a5ed2
chenp <chenpeleg@gmail.com>
2023-05-24 15:58:29 +0300

    took the compile button from the editor component

--- src/app/components/asm-editor/asm-editor.ts
+++ src/app/components/asm-editor/asm-editor.ts
@@ -1,116 +1,114 @@
-import {Component, Output, EventEmitter, ViewChild, ElementRef, Input, AfterViewInit} from "@angular/core";
+import {
+  Component,
+  Output,
+  EventEmitter,
+  ViewChild,
+  ElementRef,
+  Input,
+  AfterViewInit,
+} from "@angular/core";
 import * as _ from "lodash";
 import * as ace from "brace";
-import {Editor} from "brace";
+import { Editor } from "brace";
 
-
- // @ts-ignore
+// @ts-ignore
 ace.config.set("modePath", "./assets/js");
 
 @Component({
-    selector: "asm-editor",
-    templateUrl: "./asm-editor.html",
-  styleUrls :  ["./asm-editor.scss"]
+  selector: "asm-editor",
+  templateUrl: "./asm-editor.html",
+  styleUrls: ["./asm-editor.scss"],
 })
-export class AsmEditorComponent implements AfterViewInit
-{
-    private static ACTIVE_LINE_CLASS: string = "active-line";
+export class AsmEditorComponent implements AfterViewInit {
+  private static ACTIVE_LINE_CLASS: string = "active-line";
+  public hideAssembleButton: boolean = true;
+  @Output() compile: EventEmitter<string> = new EventEmitter<string>();
+  @Output() breakpointChange: EventEmitter<number[]> = new EventEmitter<
+    number[]
+  >();
 
-    @Output() compile: EventEmitter<string> = new EventEmitter<string>();
-    @Output() breakpointChange: EventEmitter<number[]> = new EventEmitter<number[]>();
-
-    // @ts-ignore
+  // @ts-ignore
   @ViewChild("editor") private editor: ElementRef;
-    private aceEditor: Editor | null = null  ;
+  private aceEditor: Editor | null = null;
 
-    private _breakpoints: number[] = [];
-    // @ts-ignore
-  private _activeLine: number  = null;
+  private _breakpoints: number[] = [];
 
-    get breakpoints(): number[]
-    {
-        return this._breakpoints;
+  get breakpoints(): number[] {
+    return this._breakpoints;
+  }
+
+  // @ts-ignore
+  private _activeLine: number = null;
+
+  @Input() set activeLine(value: number) {
+    if (this.aceEditor === null) {
+      return;
     }
 
-    @Input() set activeLine(value: number)
-    {
-        if (this.aceEditor === null)
-        {
-            return;
-        }
+    this.removeActiveLine();
 
-        this.removeActiveLine();
+    if (value !== null) {
+      this._activeLine = value;
+      this.aceEditor.session.addGutterDecoration(
+        value,
+        AsmEditorComponent.ACTIVE_LINE_CLASS
+      );
+      this.aceEditor.gotoLine(value + 1);
+    }
+  }
 
-        if (value !== null)
-        {
-            this._activeLine = value;
-            this.aceEditor.session.addGutterDecoration(value, AsmEditorComponent.ACTIVE_LINE_CLASS);
-            this.aceEditor.gotoLine(value + 1);
-        }
+  @Input() set text(value: string) {
+    if (this.aceEditor === null) {
+      return;
     }
 
-    @Input() set text(value: string)
-    {
-        if (this.aceEditor === null)
-        {
-            return;
-        }
+    this.aceEditor.session.getDocument().setValue(value);
+  }
 
-        this.aceEditor.session.getDocument().setValue(value);
+  ngAfterViewInit() {
+    const el = this.editor.nativeElement;
+    this.aceEditor = ace.edit(el);
+
+    this.aceEditor.session.setMode("ace/mode/assembly_x86");
+    this.aceEditor.on("guttermousedown", (e: any) => {
+      console.log(e);
+      let target = e.domEvent.target;
+      if (target.className.indexOf("ace_gutter-cell") === -1) {
+        return;
+      }
+
+      let row = e.getDocumentPosition().row;
+      this.toggleBreakpoint(row);
+      e.stop();
+    });
+  }
+
+  public emitCompile() {
+    this.compile.emit(this.aceEditor?.getValue());
+  }
+
+  private toggleBreakpoint(row: number) {
+    if (this.hasBreakpoint(row)) {
+      this.aceEditor?.session.clearBreakpoint(row);
+      _.remove(this._breakpoints, (value: number) => value === row);
+    } else {
+      this.aceEditor?.session.setBreakpoint(row, "ace_breakpoint");
+      this._breakpoints.push(row);
     }
 
-    ngAfterViewInit()
-    {
-        const el = this.editor.nativeElement;
-        this.aceEditor = ace.edit(el);
+    this.breakpointChange.emit(this._breakpoints);
+  }
 
+  private hasBreakpoint(row: number): boolean {
+    return _.includes(this._breakpoints, row);
+  }
 
-      this.aceEditor.session.setMode("ace/mode/assembly_x86");
-        this.aceEditor.on("guttermousedown", (e: any) =>
-        {
-          console.log(e)
-            let target = e.domEvent.target;
-            if (target.className.indexOf("ace_gutter-cell") === -1)
-            {
-                return;
-            }
-
-            let row = e.getDocumentPosition().row;
-            this.toggleBreakpoint(row);
-            e.stop();
-        });
+  private removeActiveLine() {
+    if (this._activeLine !== -1) {
+      this.aceEditor?.session.removeGutterDecoration(
+        this._activeLine,
+        AsmEditorComponent.ACTIVE_LINE_CLASS
+      );
     }
-
-    private toggleBreakpoint(row: number)
-    {
-        if (this.hasBreakpoint(row))
-        {
-            this.aceEditor?.session.clearBreakpoint(row);
-            _.remove(this._breakpoints, (value: number) => value === row);
-        }
-        else
-        {
-            this.aceEditor?.session.setBreakpoint(row,"ace_breakpoint");
-            this._breakpoints.push(row);
-        }
-
-        this.breakpointChange.emit(this._breakpoints);
-    }
-    private hasBreakpoint(row: number): boolean
-    {
-        return _.includes(this._breakpoints, row);
-    }
-
-    private removeActiveLine()
-    {
-        if (this._activeLine !== -1)
-        {
-            this.aceEditor?.session.removeGutterDecoration(this._activeLine, AsmEditorComponent.ACTIVE_LINE_CLASS);
-        }
-    }
-
-    public emitCompile()
-    {
-        this.compile.emit(this.aceEditor?.getValue());
-    }
+  }
 }
