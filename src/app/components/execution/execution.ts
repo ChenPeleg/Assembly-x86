c8c3d09efa367c8a7c9b9b8ee2a3fa0bb8630636
chenp <chenpeleg@gmail.com>
2023-05-25 10:11:28 +0300

    refactoring buttons

--- src/app/components/execution/execution.ts
+++ src/app/components/execution/execution.ts
@@ -1,47 +1,70 @@
-import {Component, Input, EventEmitter, Output} from "@angular/core";
-import {Process} from "../../emulation/process";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { Process } from "../../emulation/process";
+
+export type Button = "start" | "stop" | "pause" | "continue" | "step";
 
 @Component({
-    selector: "execution-controls",
-    templateUrl: "./execution.html"
+  selector: "execution-controls",
+  templateUrl: "./execution.html",
 })
-export class ExecutionComponent
-{
-    // @ts-ignore
+export class ExecutionComponent {
+  public readonly Buttons: Button[] = [
+    "start",
+    "stop",
+    "pause",
+    "continue",
+    "step",
+  ];
+  // @ts-ignore
   @Input() process: Process = null;
 
-    @Output() start: EventEmitter<Process> = new EventEmitter<Process>();
-    @Output() stop: EventEmitter<Process> = new EventEmitter<Process>();
-    @Output() continueEvent: EventEmitter<Process> = new EventEmitter<Process>();
-    @Output() pause: EventEmitter<Process> = new EventEmitter<Process>();
-    @Output() step: EventEmitter<Process> = new EventEmitter<Process>();
+  @Output() start: EventEmitter<Process> = new EventEmitter<Process>();
+  @Output() stop: EventEmitter<Process> = new EventEmitter<Process>();
+  @Output() continueEvent: EventEmitter<Process> = new EventEmitter<Process>();
+  @Output() pause: EventEmitter<Process> = new EventEmitter<Process>();
+  @Output() step: EventEmitter<Process> = new EventEmitter<Process>();
 
-  onStart()
-    {
-        this.start.emit(this.process);
-        this.process.start();
-    }
-
-  onStop()
-    {
-        this.stop.emit(this.process);
-        this.process.cpu.halt();
-    }
-    public onContinue()
-    {
-        this.continueEvent.emit(this.process);
-        this.process.cpu.run();
+  onStart() {
+    this.start.emit(this.process);
+    this.process.start();
+  }
+  getIsDisabled(button: Button): boolean {
+    if (this.process === null) {
+      return true;
     }
 
-  onPause()
-    {
-        this.pause.emit(this.process);
-        this.process.cpu.pause();
+    switch (button) {
+      case "start":
+        return this.process.isStarted();
+      case "stop":
+        return !this.process.isStarted();
+      case "pause":
+        return !this.process.isRunning();
+      case "continue":
+        return !this.process.isPaused();
+      case "step":
+        return !this.process.isPaused();
+      default:
+        return false;
     }
+  }
 
-  onStep()
-    {
-        this.step.emit(this.process);
-        this.process.cpu.step();
-    }
+  onStop() {
+    this.stop.emit(this.process);
+    this.process.cpu.halt();
+  }
+  public onContinue() {
+    this.continueEvent.emit(this.process);
+    this.process.cpu.run();
+  }
+
+  onPause() {
+    this.pause.emit(this.process);
+    this.process.cpu.pause();
+  }
+
+  onStep() {
+    this.step.emit(this.process);
+    this.process.cpu.step();
+  }
 }
