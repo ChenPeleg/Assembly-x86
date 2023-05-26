7c5ee7653ba060d2e367ff7f0f20c2ca70459506
chenp <chenpeleg@gmail.com>
2023-05-26 20:41:58 +0300

    fixed tests to run with ngrx

--- src/app/demo/counter-demo/counter-demo.component.spec.ts
+++ src/app/demo/counter-demo/counter-demo.component.spec.ts
@@ -1,23 +1,24 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { CounterDemoComponent } from "./counter-demo.component";
+import { provideMockStore } from "@ngrx/store/testing";
+import { initialState } from "../../store/counter.reducer";
 
-import { CounterDemoComponent } from './counter-demo.component';
-
-describe('CounterDemoComponent', () => {
+describe("CounterDemoComponent", () => {
   let component: CounterDemoComponent;
   let fixture: ComponentFixture<CounterDemoComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CounterDemoComponent ]
-    })
-    .compileComponents();
+      declarations: [CounterDemoComponent],
+      providers: [provideMockStore({ initialState })],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(CounterDemoComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
