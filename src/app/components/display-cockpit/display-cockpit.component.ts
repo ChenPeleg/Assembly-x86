8ad5236be029187a80c0cf02dd848fe39467dd0b
chenp <chenpeleg@gmail.com>
2023-05-28 12:52:08 +0300

    added display-cockpit.component.html the new display

--- src/app/components/display-cockpit/display-cockpit.component.ts
+++ src/app/components/display-cockpit/display-cockpit.component.ts
@@ -1,16 +1,18 @@
 import { Component } from "@angular/core";
 import { CdkDragDrop, moveItemInArray } from "@angular/cdk/drag-drop";
+import { Observable } from "rxjs";
+import { Panel, UIState } from "../../models/UIState";
+import { Store } from "@ngrx/store";
+import { UIStateActions } from "../../stores/actions/ui.state.actions";
 
-/**
- * @title Drag&Drop horizontal sorting
- */
 @Component({
   selector: "display-cockpit",
   templateUrl: "display-cockpit.component.html",
   styleUrls: ["display-cockpit.component.scss"],
 })
 export class DisplayCockpitComponent {
-  // uiPanels: Panel[];
+  count$: Observable<number>;
+
   timePeriods = [
     "Bronze age",
     "Iron age",
@@ -18,9 +20,21 @@ export class DisplayCockpitComponent {
     "Early modern period",
     "Long nineteenth century",
   ];
-  constructor() {}
+  uiState$: Observable<UIState>;
+  constructor(private store: Store<{ count: number; uiState: UIState }>) {
+    this.count$ = store.select("count");
+    this.uiState$ = store.select("uiState");
+    this.uiState$.subscribe((ui) => ui);
+  }
 
-  drop(event: CdkDragDrop<string[]>) {
-    moveItemInArray(this.timePeriods, event.previousIndex, event.currentIndex);
+  dropPanel(event: CdkDragDrop<string[]>, currentPanels: Panel[]) {
+    let newPanelArray = currentPanels.map((p, i) => ({ ...p, order: i + 1 }));
+    moveItemInArray(newPanelArray, event.previousIndex, event.currentIndex);
+    newPanelArray = newPanelArray.map((p, i) => ({ ...p, order: i + 1 }));
+    this.store.dispatch(UIStateActions.reorder({ panels: newPanelArray }));
+  }
+  clickVisibility($event: MouseEvent, panel: Panel) {
+    const newPanel = { ...panel, isVisible: !panel.isVisible };
+    this.store.dispatch(UIStateActions.changeVisibility(newPanel));
   }
 }
