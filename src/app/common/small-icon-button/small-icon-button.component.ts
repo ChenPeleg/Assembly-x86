9b64087c7dc01474bc07dd80799064fd05776ab0
chenp <chenpeleg@gmail.com>
2023-05-28 11:59:31 +0300

    added button

---
+++ src/app/common/small-icon-button/small-icon-button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-small-icon-button',
+  templateUrl: './small-icon-button.component.html',
+  styleUrls: ['./small-icon-button.component.scss']
+})
+export class SmallIconButtonComponent {
+
+}
