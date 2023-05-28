f55bb8fbe6c2fcf077079a268c19e49e7db6aa6a
chenp <chenpeleg@gmail.com>
2023-05-28 10:35:18 +0300

    changed store to stores

--- src/app/demo/counter-demo/counter-demo.component.ts
+++ src/app/demo/counter-demo/counter-demo.component.ts
@@ -1,7 +1,7 @@
 import { Component } from "@angular/core";
 import { Store } from "@ngrx/store";
 import { Observable } from "rxjs";
-import { decrement, increment, reset } from "../../store/counter.actions";
+import { decrement, increment, reset } from "../../stores/counter.actions";
 
 @Component({
   selector: "app-counter-demo",
