48edf81fa9f0e32a6340e52846ef78888caa2426
chenp <chenpeleg@gmail.com>
2023-05-28 13:43:57 +0300

    fixed tests

--- src/app/common/small-icon-button/small-icon-button.component.spec.ts
+++ src/app/common/small-icon-button/small-icon-button.component.spec.ts
@@ -1,23 +1,38 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { SmallIconButtonComponent } from './small-icon-button.component';
+import { SmallIconButtonComponent } from "./small-icon-button.component";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatCardModule } from "@angular/material/card";
+import { MatButtonToggleModule } from "@angular/material/button-toggle";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatTreeModule } from "@angular/material/tree";
+import { MatTooltipModule } from "@angular/material/tooltip";
 
-describe('SmallIconButtonComponent', () => {
+describe("SmallIconButtonComponent", () => {
   let component: SmallIconButtonComponent;
   let fixture: ComponentFixture<SmallIconButtonComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SmallIconButtonComponent ]
-    })
-    .compileComponents();
+      imports: [
+        MatToolbarModule,
+        MatCardModule,
+        MatButtonToggleModule,
+        MatButtonModule,
+        MatIconModule,
+        MatTreeModule,
+        MatTooltipModule,
+      ],
+      declarations: [SmallIconButtonComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(SmallIconButtonComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
