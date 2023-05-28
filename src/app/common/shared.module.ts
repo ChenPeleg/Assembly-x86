e0d278f3af440ace9fc776fd5cb28123473f14a7
chenp <chenpeleg@gmail.com>
2023-05-28 12:01:28 +0300

    added shared module

---
+++ src/app/common/shared.module.ts
@@ -0,0 +1,7 @@
+import { NgModule } from "@angular/core";
+import { SmallIconButtonComponent } from "./small-icon-button/small-icon-button.component";
+
+@NgModule({
+  declarations: [SmallIconButtonComponent],
+})
+export class SharedModule {}
