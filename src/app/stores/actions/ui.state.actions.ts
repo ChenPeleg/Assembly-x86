7a4151c55dc874ffae90dd594dd77adb7350956b
chenp <chenpeleg@gmail.com>
2023-05-28 11:01:48 +0300

    added change visiablility reducer

--- src/app/stores/actions/ui.state.actions.ts
+++ src/app/stores/actions/ui.state.actions.ts
@@ -1,9 +1,11 @@
 import { createAction, props } from "@ngrx/store";
 import { Panel, UIState } from "../../models/UIState";
 
-export const reorder = createAction("[UI Component] Reorder", props<UIState>());
-export const changeVisibility = createAction(
-  "[UI Component] Change Visibility",
-  props<Panel>()
-);
-export const resetToDefault = createAction("[UI Component] reset To Default");
+export const UIStateActions = {
+  reorder: createAction("[UI Component] Reorder", props<UIState>()),
+  changeVisibility: createAction(
+    "[UI Component] Change Visibility",
+    props<Panel>()
+  ),
+  resetToDefault: createAction("[UI Component] reset To Default"),
+};
---
+++ src/app/stores/reducers/ui.state.reducer.ts
@@ -0,0 +1,25 @@
+import { createReducer, on } from "@ngrx/store";
+import { UIState } from "../../models/UIState";
+import { UIStateActions } from "../actions/ui.state.actions";
+
+export const UIStateInitialState: UIState = {
+  panels: [
+    { isVisible: true, name: "console", order: 1 },
+    { isVisible: true, name: "cpu", order: 2 },
+    { isVisible: true, name: "memory", order: 3 },
+  ],
+  theme: "default",
+};
+
+export const UIStateReducer = createReducer(
+  UIStateInitialState,
+  on(UIStateActions.reorder, (state, { panels }) => ({
+    ...state,
+    panels,
+  })),
+  on(UIStateActions.changeVisibility, (state, { panel }) => ({
+    ...state,
+    // @ts-ignore
+    panels: state.panels.map((p) => p),
+  }))
+);
