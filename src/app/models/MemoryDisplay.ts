295e3c2a20af441a4e2b8c5f3abf2576cd2fb562
chenp <chenpeleg@gmail.com>
2023-06-08 23:16:13 +0300

    added memory value types

--- src/app/models/MemoryDisplay.ts
+++ src/app/models/MemoryDisplay.ts
@@ -1,4 +1,6 @@
+export type MemoryValueType = "number" | "ascii" | "binary" | "hex";
+
 export interface MemoryDisplay {
   wordSize: 1 | 2 | 4;
-  valueType: "number" | "ascii" | "binary" | "hex";
+  valueType: MemoryValueType;
 }
