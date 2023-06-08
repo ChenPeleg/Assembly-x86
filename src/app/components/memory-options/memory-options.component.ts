6efce07b01490bb0257898db6e9087b540f4b810
chenp <chenpeleg@gmail.com>
2023-06-08 22:22:31 +0300

    styled memory sizes

---
+++ src/app/components/memory-options/memory-options.component.ts
@@ -0,0 +1,9 @@
+import { Component, ViewEncapsulation } from "@angular/core";
+
+@Component({
+  selector: "memory-options",
+  templateUrl: "./memory-options.component.html",
+  styleUrls: ["./memory-options.component.scss"],
+  encapsulation: ViewEncapsulation.None,
+})
+export class MemoryOptionsComponent {}
