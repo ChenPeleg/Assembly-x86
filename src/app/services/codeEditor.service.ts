70718ed4cdf4c5e8a070958526c65724f014178e
chenp <chenpeleg@gmail.com>
2023-06-17 17:55:10 +0300

    added typeof code to service

--- src/app/services/codeEditor.service.ts
+++ src/app/services/codeEditor.service.ts
@@ -1,12 +1,18 @@
 import { Injectable } from "@angular/core";
 import { CodeEditorState } from "../models/CodeEditorState";
 import { Subject } from "rxjs";
+import { TypeOfCodeInEditor } from "../models/TypeOfCodeInEditor";
 
 @Injectable()
 export class CodeEditorService {
-  $editorCodeUpdated: Subject<CodeEditorState> = new Subject<CodeEditorState>();
+  $editorCodeUpdater: Subject<CodeEditorState> = new Subject<CodeEditorState>();
+  typeOfCode: TypeOfCodeInEditor = TypeOfCodeInEditor.Default;
   constructor() {}
   public updateCodeEditor(codeEditorState: CodeEditorState) {
-    this.$editorCodeUpdated.next(codeEditorState);
+    this.typeOfCode = codeEditorState.typeOfCode;
+    this.$editorCodeUpdater.next(codeEditorState);
+  }
+  public updateCodeChangesTracker(codeEditorState: string | undefined) {
+    console.log(codeEditorState);
   }
 }
