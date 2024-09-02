ed4623f579567b1dc9ad6122ea0e7c872ad8d5f8
Chen Peleg <chenpeleg@gmail.com>
2024-09-02 13:51:48 +0300

    a function that defines multiple tags

--- src/app/components/code/code-mirror-handler/code-mirror-handler.component.ts
+++ src/app/components/code/code-mirror-handler/code-mirror-handler.component.ts
@@ -1,7 +1,13 @@
 import { AfterViewInit, Component, Inject, ViewChild } from "@angular/core";
 import { basicSetup } from "codemirror";
-import { EditorState, Extension } from "@codemirror/state";
-import { EditorView } from "@codemirror/view";
+import { EditorState, Extension, RangeSetBuilder } from "@codemirror/state";
+import {
+  Decoration,
+  DecorationSet,
+  EditorView,
+  ViewPlugin,
+  ViewUpdate,
+} from "@codemirror/view";
 import { DOCUMENT } from "@angular/common";
 
 @Component({
@@ -16,9 +22,62 @@ export class CodeMirrorHandlerComponent implements AfterViewInit {
 
   constructor(@Inject(DOCUMENT) private document: Document) {}
 
+  addMultipleTags(): Extension {
+    return ViewPlugin.fromClass(
+      class {
+        decorations: DecorationSet;
+
+        constructor(view: EditorView) {
+          this.decorations = this.buildDecorations(view);
+        }
+
+        update(update: ViewUpdate) {
+          if (update.docChanged || update.viewportChanged) {
+            this.decorations = this.buildDecorations(update.view);
+          }
+        }
+
+        buildDecorations(view: EditorView): DecorationSet {
+          let builder = new RangeSetBuilder<Decoration>();
+          for (let { from, to } of view.visibleRanges) {
+            let text = view.state.doc.sliceString(from, to);
+            let tagPositions = this.findTagPositions(text);
+            for (let pos of tagPositions) {
+              builder.add(
+                from + pos.start,
+                from + pos.end,
+                Decoration.mark({ class: pos.class })
+              );
+            }
+          }
+          return builder.finish();
+        }
+
+        findTagPositions(
+          text: string
+        ): { start: number; end: number; class: string }[] {
+          let positions = [];
+          let regex = /lazer|anotherTag|exampleTag/g;
+          let match;
+          while ((match = regex.exec(text)) !== null) {
+            positions.push({
+              start: match.index,
+              end: match.index + match[0].length,
+              class: `${match[0]}-tag`,
+            });
+          }
+          return positions;
+        }
+      },
+      {
+        decorations: (v) => v.decorations,
+      }
+    );
+  }
+
   ngAfterViewInit(): void {
     let myEditorElement = this.myEditor.nativeElement;
-    let myExt: Extension = [basicSetup];
+    let myExt: Extension = [basicSetup, this.addMultipleTags()];
     let state!: EditorState;
 
     try {
@@ -27,13 +86,8 @@ export class CodeMirrorHandlerComponent implements AfterViewInit {
         extensions: myExt,
       });
     } catch (e) {
-      //Please make sure install codemirror@6.0.1
-      //If your command was npm install codemirror, will installed 6.65.1(whatever)
-      //You will be here.
       console.error(e);
     }
-
-    //console.log(state);
 
     let view = new EditorView({
       state,
