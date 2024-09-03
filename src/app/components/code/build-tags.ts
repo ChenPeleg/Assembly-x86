13510f511a1a405c32a922f8d67866a6c7db3a30
Chen Peleg <chenpeleg@gmail.com>
2024-09-03 10:43:23 +0300

    matching works

--- src/app/components/code/build-tags.ts
+++ src/app/components/code/build-tags.ts
@@ -8,9 +8,13 @@ import {
 } from "@codemirror/view";
 
 export interface CodeMirrorTag {
+  /**
+   * @description Names of the tags seperated by a dot
+   */
   name: string;
   regex: RegExp;
   order: number;
+  caseSensitive: boolean;
 }
 
 export function addMultipleTags(tags: CodeMirrorTag[]): Extension {
@@ -50,112 +54,27 @@ export function addMultipleTags(tags: CodeMirrorTag[]): Extension {
       findTagPositions(
         text: string
       ): { start: number; end: number; class: string }[] {
-        let positions = [];
+        let positions: {
+          start: number;
+          end: number;
+          class: string;
+        }[] = [];
         for (let tag of tags) {
           let match;
-          while ((match = tag.regex.exec(text)) !== null) {
+          const allMatches = [...text.matchAll(new RegExp(tag.regex, "g"))];
+          console.log(allMatches);
+          allMatches.forEach((match) => {
+            if (!match || match.index === undefined) {
+              return;
+            }
             positions.push({
               start: match.index,
               end: match.index + match[0].length,
-              class: `${tag.name}-tag`,
+              class: `${tag.name}`,
             });
-          }
+          });
         }
         return positions;
-      }
-      //
-      // token(stream: any, state: any): string | null {
-      //   console.log("stream", stream);
-      //   if (state.tokenize) {
-      //     return state.tokenize(stream, state);
-      //   }
-      //
-      //   if (stream.eatSpace()) {
-      //     return null;
-      //   }
-      //
-      //   let style: string | null = null;
-      //   let cur: string;
-      //   let ch = stream.next();
-      //
-      //   if (ch === "/") {
-      //     if (stream.eat("*")) {
-      //       state.tokenize = this.clikeComment;
-      //       return this.clikeComment(stream, state);
-      //     }
-      //   }
-      //
-      //   if (ch === "#") {
-      //     stream.skipToEnd();
-      //     return "comment";
-      //   }
-      //
-      //   if (ch === '"') {
-      //     this.nextUntilUnescaped(stream, '"');
-      //     return "string";
-      //   }
-      //
-      //   if (ch === ".") {
-      //     stream.eatWhile(/\w/);
-      //     cur = stream.current().toLowerCase();
-      //     style = "builtin"; // Assuming directives are built-in
-      //     return style || null;
-      //   }
-      //
-      //   if (ch === "=") {
-      //     stream.eatWhile(/\w/);
-      //     return "tag";
-      //   }
-      //
-      //   if (ch === "{" || ch === "}") {
-      //     return "bracket";
-      //   }
-      //
-      //   if (/\d/.test(ch)) {
-      //     if (ch === "0" && stream.eat("x")) {
-      //       stream.eatWhile(/[0-9a-fA-F]/);
-      //       return "number";
-      //     }
-      //     stream.eatWhile(/\d/);
-      //     return "number";
-      //   }
-      //
-      //   if (/\w/.test(ch)) {
-      //     stream.eatWhile(/\w/);
-      //     if (stream.eat(":")) {
-      //       return "tag";
-      //     }
-      //     cur = stream.current().toLowerCase();
-      //     style = "variable"; // Assuming registers are variables
-      //     return style || null;
-      //   }
-      //
-      //   return null;
-      // }
-
-      nextUntilUnescaped(stream: any, end: string): boolean {
-        let escaped = false,
-          next;
-        while ((next = stream.next()) != null) {
-          if (next === end && !escaped) {
-            return false;
-          }
-          escaped = !escaped && next === "\\";
-        }
-        return escaped;
-      }
-
-      clikeComment(stream: any, state: any): string {
-        let maybeEnd = false,
-          ch;
-        while ((ch = stream.next()) != null) {
-          if (ch === "/" && maybeEnd) {
-            state.tokenize = null;
-            break;
-          }
-          maybeEnd = ch === "*";
-        }
-        return "comment";
       }
     },
     {
