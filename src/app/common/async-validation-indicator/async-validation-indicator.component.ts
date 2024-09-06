fc1029af3abf56894ed8de23464cc56c3e44be9e
Chen Peleg <chenpeleg@gmail.com>
2024-09-06 12:22:13 +0300

    added another type of spinner

--- src/app/common/async-validation-indicator/async-validation-indicator.component.ts
+++ src/app/common/async-validation-indicator/async-validation-indicator.component.ts
@@ -51,7 +51,19 @@ export enum GeneralFieldValidationStatus {
       class="async-validation-icon-wrapper"
     >
       <div class="text-blue-600">
-        {{ "{...}" }}
+        <svg
+          fill="#5f6368"
+          height="24px"
+          viewBox="0 -960 960 960"
+          width="24px"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M320-240 80-480l240-240 57 57-184 184 183 183-56 56Zm320 0-57-57 184-184-183-183 56-56 240 240-240 240Z"
+          />
+        </svg>
+
+        <!--        {{ "{...}" }}-->
         <!--        <img-->
         <!--          ngSrc="../../../assets/svg/form-error-icon.svg"-->
         <!--          alt="error"-->
