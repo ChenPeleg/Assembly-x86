255591f65f64157e7cffa24fda8eb06d52893f6e
Chen Peleg <chenpeleg@gmail.com>
2024-09-06 11:21:18 +0300

    added check mark to component

--- src/app/common/shared.module.ts
+++ src/app/common/shared.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from "@angular/core";
 import { SmallIconButtonComponent } from "./small-icon-button/small-icon-button.component";
 import { AllMatModules } from "../app.module";
+import { AsyncValidationIndicatorComponent } from "./async-validation-indicator/async-validation-indicator.component";
+import { SpinnerWithCheckMarkComponent } from "./spinner-with-check-mark/spinner-with-check-mark.component";
+import { NgClass, NgStyle } from "@angular/common";
 
 @NgModule({
-  imports: [...AllMatModules],
-  declarations: [SmallIconButtonComponent],
-  exports: [SmallIconButtonComponent],
+  imports: [...AllMatModules, NgClass, NgStyle],
+  declarations: [
+    SmallIconButtonComponent,
+    AsyncValidationIndicatorComponent,
+    SpinnerWithCheckMarkComponent,
+  ],
+  exports: [
+    SmallIconButtonComponent,
+    AsyncValidationIndicatorComponent,
+    SpinnerWithCheckMarkComponent,
+  ],
 })
 export class SharedModule {}
