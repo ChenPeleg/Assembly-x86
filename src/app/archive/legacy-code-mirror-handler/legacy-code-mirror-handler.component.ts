e0f5ef4ae82ee109871344414641a2ccce7928c1
Chen Peleg <chenpeleg@gmail.com>
2024-09-06 10:14:49 +0300

    refactored legacy component

--- src/app/archive/legacy-code-mirror-handler/legacy-code-mirror-handler.component.ts
+++ src/app/archive/legacy-code-mirror-handler/legacy-code-mirror-handler.component.ts
@@ -9,22 +9,22 @@ import { basicSetup } from "codemirror";
 import { EditorState, Extension } from "@codemirror/state";
 import { EditorView } from "@codemirror/view";
 import { DOCUMENT } from "@angular/common";
-import { addMultipleTags } from "../addons/build-tags";
-import { asmTagList } from "../tag-list";
-import { defaultCodeText } from "../../../stores/reducers/code-editor.reducer";
-import { BuildBreakPointGutterExtension } from "../addons/gutter-breakpoints";
-import { customCMTheme } from "../addons/code-mirror-theme";
-import { getFoldingRangesByIndent } from "../addons/gutter-fold-code";
+import { addMultipleTags } from "../../components/code/addons/build-tags";
+import { asmTagList } from "../../components/code/tag-list";
+import { defaultCodeText } from "../../stores/reducers/code-editor.reducer";
+import { BuildBreakPointGutterExtension } from "../../components/code/addons/gutter-breakpoints";
+import { customCMTheme } from "../../components/code/addons/code-mirror-theme";
+import { getFoldingRangesByIndent } from "../../components/code/addons/gutter-fold-code";
 import { foldService } from "@codemirror/language";
-import { customFoldGutter } from "../addons/gutter-fold-custom-marker";
+import { customFoldGutter } from "../../components/code/addons/gutter-fold-custom-marker";
 
 @Component({
   selector: "code-mirror-handler",
-  templateUrl: "./code-mirror-handler.component.html",
-  styleUrls: ["./code-mirror-handler.component.scss"],
+  templateUrl: "./legacy-code-mirror-handler.component.html",
+  styleUrls: ["./legacy-code-mirror-handler.component.scss"],
   encapsulation: ViewEncapsulation.None,
 })
-export class CodeMirrorHandlerComponent implements AfterViewInit {
+export class LegacyCodeMirrorHandlerComponent implements AfterViewInit {
   public title = "component-overview";
   @ViewChild("myeditor") myEditor: any;
   private breakpoints: { [key: number]: boolean } = {};
