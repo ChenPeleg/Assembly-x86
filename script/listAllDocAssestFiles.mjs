005f666641f33a3b3cf15202f455d6b8eaf1f5eb
Chen Peleg <chenpeleg@gmail.com>
2024-09-27 12:58:01 +0300

    refactor pages names to pages list

--- script/listAllDocAssestFiles.mjs
+++ script/listAllDocAssestFiles.mjs
@@ -3,41 +3,62 @@ import { writeFileSync } from "fs";
 import { readFile } from "node:fs/promises";
 import * as path from "path";
 
-export const readFileAsync = async (filePath) => {
-  return await readFile( filePath , "utf8");
+class ListAllDocsFilesHelper {
+  static async readFileAsync(filePath) {
+    return await readFile(filePath, "utf8");
+  }
+
+  static async getDirFiles(baseDir, testFilesNamesPattern) {
+    const testFilesNamesRegex = new RegExp(`.*${testFilesNamesPattern}`, "gi");
+
+    const recursiveGetAllTestFiles = (dir, allFiles) => {
+
+      fs.readdirSync(dir)
+
+        .forEach((elm, i) => {
+          const fullFileName = path.join(dir, elm);
+          if (fs.lstatSync(fullFileName).isDirectory()) {
+            return recursiveGetAllTestFiles(fullFileName, allFiles);
+          }
+          if (elm.match(testFilesNamesRegex)) {
+            allFiles.push(fullFileName);
+
+          }
+        });
+      return allFiles;
+    };
+    return recursiveGetAllTestFiles(path.resolve(baseDir), []).map(f => f.replace(baseDir, ""));
+  };
+
+  static async getCustomDocIdFromFile(filePath, docsPath) {
+    const fileContents = await ListAllDocsFilesHelper.readFileAsync(path.join(docsPath, filePath));
+    const customDocId = fileContents.match(/<!--\s*customDocId:\s*(\w+)\s*-->/);
+    return customDocId ? customDocId[1] : "";
+  }
+
+  static async addCustomDocIdToFilePathName(filePath, docsPath) {
+    const customDocId = ListAllDocsFilesHelper.getCustomDocIdFromFile(filePath, docsPath);
+    return `${filePath} <${customDocId}>`;
+  }
+  static async addCustomIdsToAllFilesInList(filePaths, docsPath) {
+    const filePathsWithCustomIds = [...filePaths];
+    for (const file of filePathsWithCustomIds) {
+      const customDocId = await  ListAllDocsFilesHelper.getCustomDocIdFromFile(file, docsPath);
+      const newFilePath = `${file} <${customDocId}>`;
+      const index = filePathsWithCustomIds.indexOf(file);
+      filePathsWithCustomIds[index] = newFilePath;
+    }
+    return filePathsWithCustomIds;
+  }
 }
 
-export const getDirFiles = async (baseDir, testFilesNamesPattern) => {
-  const testFilesNamesRegex = new RegExp(`.*${testFilesNamesPattern}`, "gi");
-
-  const recursiveGetAllTestFiles = (dir, allFiles) => {
-
-    fs.readdirSync(dir)
-
-      .forEach((elm, i) => {
-        const fullFileName = path.join(dir, elm);
-        if (fs.lstatSync(fullFileName).isDirectory()) {
-          return recursiveGetAllTestFiles(fullFileName, allFiles);
-        }
-        if (elm.match(testFilesNamesRegex)) {
-          allFiles.push(fullFileName);
-
-        }
-      });
-    return allFiles;
-  };
-  return recursiveGetAllTestFiles(path.resolve(baseDir), []).map(f => f.replace(baseDir, ""));
-};
 
 const listAllDocAssetFiles = async () => {
   const docsPath = path.resolve("src", "assets", "documentation");
-  const filePaths = await getDirFiles(docsPath, ".md");
-  for (const file of filePaths) {
-    const fileContents = await readFileAsync(path.join(docsPath, file ));
-    const customDocId = fileContents.match(/<!--\s*customDocId:\s*(\w+)\s*-->/);
-  }
+  const filePaths = await ListAllDocsFilesHelper.getDirFiles(docsPath, ".md");
+ const filePathsWithCustomIds =  await ListAllDocsFilesHelper.addCustomIdsToAllFilesInList(filePaths, docsPath);
 
-  const filesWithForwardSlash = filePaths.map(f => f.replace(/\\/g, "/"));
+  const filesWithForwardSlash = filePathsWithCustomIds.map(f => f.replace(/\\/g, "/"));
 
   writeFileSync(path.resolve(docsPath, "doclist.txt"), filesWithForwardSlash.join("\n"));
 
